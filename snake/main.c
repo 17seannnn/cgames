c9128baf852690233df44351c4f5f6c92844d5b6
17seannnn <17sean@protonmail.com>
2021-10-31 17:41:01 +0300

    feat: add tail when eat apple

--- snake/main.c
+++ snake/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include <curses.h>
 
 #include "config.h"
@@ -10,12 +11,15 @@ const char help_text[] = "";
 const char version_text[] = "";
 
 enum {
+        key_escape = 27,
+
         map_height = 16,
         map_width  = 50,
 
+        delay_duration = 100,
+
         hor_symb   = '-',
         ver_symb   = '|',
-
         apple_symb = '@'
 };
 
@@ -25,7 +29,7 @@ struct map {
 
 struct tail {
         int cur_x, cur_y, dx, dy;
-        struct tail *next;
+        struct tail *prev;
 };
 
 struct apple {
@@ -73,22 +77,37 @@ void initmap(struct map *m)
         m->max_y = m->min_y + map_height - 1;
 }
 
+void add_tail(struct tail **s, int x, int y)
+{
+        struct tail *t = malloc(sizeof(*t));
+        t->cur_x = x;
+        t->cur_y = y;
+        t->prev = *s;
+        if (t->prev) {
+                t->dx = t->prev->dx;
+                t->dy = t->prev->dy;
+        } else {
+                t->dx = 0;
+                t->dy = 0;
+        }
+        *s = t;
+}
+
 void initsnake(struct tail **s, struct map m)
 {
         struct tail *t;
-        for (t = *s; *s; s = &(*s)->next)
+        while (*s) {
+                t = *s;
+                s = &(*s)->prev;
                 free(t);
-        *s = malloc(sizeof(**s));
-        (*s)->cur_x = (m.min_x + m.max_x) / 2;
-        (*s)->cur_y = (m.min_y + m.max_y) / 2;
-        (*s)->dx = 0;
-        (*s)->dy = 0;
-        (*s)->next = NULL;
+        }
+        *s = NULL;
+        add_tail(s, (m.min_x + m.max_x) / 2, (m.min_y + m.max_y) / 2);
 }
 
 int is_empty(int x, int y, struct tail *s)
 {
-        for (; s; s = s->next)
+        for (; s; s = s->prev)
                 if (x == s->cur_x && y == s->cur_y)
                         return 0;
         return 1;
@@ -101,10 +120,14 @@ void initapple(struct apple *a, struct tail *s, struct map m)
                 x = m.min_x + rrand(map_width-2) + 1;
                 y = m.min_y + rrand(map_height-2) + 1;
         } while (!is_empty(x, y, s));
+        a->cur_x = x;
+        a->cur_y = y;
 }
 
 void initgame(struct map *m, struct tail **s, struct apple *a)
 {
+        srand(time(NULL));
+        timeout(delay_duration);
         initmap(m);
         initsnake(s, *m);
         initapple(a, *s, *m);
@@ -127,40 +150,126 @@ void show_map(struct map m)
 
 void show_snake(struct tail *s)
 {
-        for (; s; s = s->next)
+        for (; s; s = s->prev)
                 mvaddch(s->cur_y, s->cur_x, s->dx ? hor_symb : ver_symb);
+        refresh();
 }
 
 void hide_snake(struct tail *s)
 {
-        for (; s; s = s->next)
+        for (; s; s = s->prev)
                 mvaddch(s->cur_y, s->cur_x, ' ');
+        refresh();
+}
+
+void set_direction(struct tail *s, int dx, int dy)
+{
+        if (s->prev)
+                set_direction(s->prev, s->dx, s->dy);
+        s->dx = dx;
+        s->dy = dy;
+}
+
+void check(int *coord, int min, int max)
+{
+        if (*coord < min)
+                *coord = max;
+        if (*coord > max)
+                *coord = min;
+}
+
+void move_snake(struct tail *s, struct map m)
+{
+        struct tail *t = s;
+        hide_snake(s);
+        for (; t; t = t->prev) {
+                t->cur_x += t->dx;
+                t->cur_y += t->dy;
+                check(&t->cur_x, m.min_x+1, m.max_x-1);
+                check(&t->cur_y, m.min_y+1, m.max_y-1);
+        }
+        show_snake(s);
 }
 
 void show_apple(struct apple a)
 {
         mvaddch(a.cur_y, a.cur_x, apple_symb);
+        refresh();
 }
 
 void hide_apple(struct apple a)
 {
         mvaddch(a.cur_y, a.cur_x, ' ');
+        refresh();
+}
+
+int move_apple(struct apple *a, struct tail *s, struct map m)
+{
+        initapple(a, s, m);
+        show_apple(*a);
+        return 1;
+}
+
+/* TODO check if crash itself tail */
+int check_collision(struct tail *s, struct apple a)
+{
+        if (s->cur_x + s->dx == a.cur_x && s->cur_y + s->dy == a.cur_y)
+                return -1;
+        return 1;
 }
 
 void draw_screen(struct map m, struct tail *s, struct apple a)
 {
+        clear();
         show_map(m);
         show_snake(s);
         show_apple(a);
 }
 
-void playgame(struct map m, struct tail *s, struct apple *a)
+void handle_resize()
 {
 
 }
 
+void playgame(struct map *m, struct tail **s, struct apple *a)
+{
+        int key, res;
+        while ((key = getch()) != key_escape) {
+                switch (key) {
+                case KEY_UP:
+                        set_direction(*s, 0, -1);
+                        break;
+                case KEY_DOWN:
+                        set_direction(*s, 0, 1);
+                        break;
+                case KEY_LEFT:
+                        set_direction(*s, -1, 0);
+                        break;
+                case KEY_RIGHT:
+                        set_direction(*s, 1, 0);
+                        break;
+                case KEY_RESIZE:
+                        handle_resize();
+                        break;
+                default:
+                        set_direction(*s, (*s)->dx, (*s)->dy);
+                }
+                move_snake(*s, *m);
+                res = check_collision(*s, *a);
+                if (res < 0) {
+                        add_tail(s, a->cur_x, a->cur_y);
+                        res = move_apple(a, *s, *m);
+                        if (!res)
+                                break;
+                } else if (!res) {
+                        break;
+                }
+        }
+}
+
 int ask_continue()
 {
+        clear();
         return 0;
 }
 
@@ -175,8 +284,7 @@ int main(int argc, char **argv)
         do {
                 initgame(&m, &s, &a);
                 draw_screen(m, s, a);
-                sleep(2);
-                playgame(m, s, &a);
+                playgame(&m, &s, &a);
         } while (ask_continue());
         endwin();
         return 0;
