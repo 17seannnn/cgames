ef8f14e2eac4d39467eb970b3e814849258172c5
17seannnn <17sean@protonmail.com>
2021-11-10 15:22:43 +0300

    build: rename snake dir

--- csnake/main.c
+++ csnake/main.c
@@ -10,7 +10,7 @@
 #define PROGRAM_NAME "csnake"
 #define PACKAGE_NAME "cgames"
 #define PACKAGE_PAGE "https://github.com/17seannnn/cgames"
-#define VERSION "???"
+#define VERSION "1.00"
 
 #define LICENSE "GPLv3"
 #define LICENSE_PAGE "https://gnu.org/licenses/gpl.html"
