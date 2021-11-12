0154010f22a88bf0607ce8965bc46fc2138347d4
17seannnn <17sean@protonmail.com>
2021-11-12 18:18:24 +0300

    build: upgrade build.sh

--- csnake/build.sh
+++ csnake/build.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 
-gcc -ansi -pedantic -Wall -g -Og -lcurses main.c -o csnake
+CC=gcc
+NAME=csnake
+BINDIR=.
+#BINDIR=/usr/local/bin
+LOCALEDIR=.
+#LOCALEDIR=/usr/share/locale/
+
+case $1 in
+        install)
+                cd src/;
+                $CC -ansi -pedantic -Wall -Og -g -lcurses main.c -o ../$NAME
+                cd ..;
+                mv -f $NAME $BINDIR
+                mkdir -p $LOCALEDIR/ru/LC_MESSAGES;
+                msgfmt po/ru.po -o $LOCALEDIR/ru/LC_MESSAGES/$NAME.mo;;
+        pot) xgettext -k="_" -f "po/POTFILES.in" -D "src" -o po/$NAME.pot;;
+        mo)
+                mkdir -p $LOCALEDIR/ru/LC_MESSAGES;
+                msgfmt po/ru.po -o $LOCALEDIR/ru/LC_MESSAGES/$NAME.mo;;
+        * | --help)
+                echo "\
+Usage:
+build.sh [COMMAND/--OPTION]
+
+Options:
+        --help     this help
+
+Commands:
+        install    install this program
+        pot        generate .pot file in po/
+        mo         compile and move .mo files from po/ to locale dir";;
+esac
