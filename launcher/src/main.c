9c06db28b12add02b80dfb7938b383abf0241c6b
17seannnn <17sean@protonmail.com>
2022-01-26 22:22:44 +0300

    feat: add options

--- launcher/src/main.c
+++ launcher/src/main.c
@@ -42,6 +42,42 @@ const int   mm_colors[mm_colors_count] = {
 };
 const int settings_menu = 0;
 
+static const char program_name[]      = N_("launcher");
+static const char program_name_long[] = N_("cgames");
+static const char program_page[]      = N_("https://github.com/17seannnn/cgames");
+static const char version[]           = N_("3");
+
+static const char copyright_year[] = N_("2021");
+static const char license[]        = N_("GPLv3");
+static const char license_page[] = N_("https://www.gnu.org/licenses/gpl-3.0.en.html");
+
+static const char author[]          = gettext_noop("Sergey Nikonov");
+static const char author_nickname[] = N_("17seannnn");
+static const char author_page[]     = N_("https://github.com/17seannnn");
+
+static const char help_opt_short[]    = N_("-h");
+static const char help_opt_long[]     = N_("--help");
+static const char version_opt_short[] = N_("-V");
+static const char version_opt_long[]  = N_("--version");
+
+static const char help_text[] = gettext_noop("\
+Usage: %s [-OPT/--OPT] file1 file2...\n\
+\n\
+Options\n\
+\n\
+%s, %s      show help\n\
+%s, %s   show version\n\
+\n\
+Report bugs to & %s home page: <%s>\n");
+
+static const char version_text[] = gettext_noop("\
+%s (%s) %s\n\
+Copyright (C) %s %s (%s)\n\
+License %s: <%s>\n\
+\n\
+Written by %s (%s).\n\
+Github: <%s>\n");
+
 static void initgettext()
 {
         char localdir[bufsize], localfile[bufsize];
@@ -64,6 +100,41 @@ static void initgettext()
         textdomain(TEXTDOMAIN);
 }
 
+static void show_help()
+{
+        printf(_(help_text), program_name,
+                             help_opt_short, help_opt_long,
+                             version_opt_short, version_opt_long,
+                             program_name, program_page);
+}
+
+static void show_version()
+{
+        printf(_(version_text), program_name, program_name_long, version,
+                                copyright_year, _(author), author_nickname,
+                                license, license_page,
+                                author, author_nickname,
+                                author_page);
+}
+
+static int handle_opt(const char **argv)
+{
+        argv++;
+        for ( ; *argv; argv++) {
+                if (0 == strcmp(*argv, help_opt_short) ||
+                    0 == strcmp(*argv, help_opt_long)) {
+                        show_help();
+                        return 0;
+                }
+                else if (0 == strcmp(*argv, version_opt_short) ||
+                         0 == strcmp(*argv, version_opt_long)) {
+                        show_version();
+                        return 0;
+                }
+        }
+        return 1;
+}
+
 static void mainmenu_handle(int res)
 {
         switch (res) {
@@ -78,10 +149,13 @@ static void mainmenu_handle(int res)
         }
 }
 
-int main()
+int main(int argc, char **argv)
 {
         int res;
         initgettext();
+        res = handle_opt((const char **)argv);
+        if (!res)
+                return 0;
         for (;;) {
                 initscr();
                 res = mainmenu();
