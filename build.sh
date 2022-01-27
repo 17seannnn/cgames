427fb64992ca75778d20ef78a9099a66789a8941
17seannnn <17sean@protonmail.com>
2022-01-27 11:12:40 +0300

    build: move sources in one dir

--- build.sh
+++ build.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
-cd launcher
-./build.sh install
-cd ../cr
-./build.sh install
-cd ../csnake
-./build.sh install
+CC=gcc
+NAME=cgames
+if sudo -v 2> /dev/null; then
+        BINDIR=/usr/bin
+        LOCALEDIR=/usr/share/locale/
+else
+        BINDIR=~/.local/bin
+        LOCALEDIR=~/.local/share/locale/
+fi
+
+case $1 in
+        install)
+                cd src
+                $CC -ansi -pedantic -Wall -Og -g -lncursesw -c mainmenu.c cr.c csnake.c
+                $CC -ansi -pedantic -Wall -Og -g -lncursesw mainmenu.o cr.o csnake.o main.c -o ../$NAME
+                cd ..
+                msgfmt po/ru.po -o $NAME.mo
+                mkdir -p $BINDIR
+                mv -f $NAME $BINDIR
+                mkdir -p $LOCALEDIR/ru/LC_MESSAGES
+                mv $NAME.mo $LOCALEDIR/ru/LC_MESSAGES;;
+        pot) xgettext -k="_" -f "po/POTFILES.in" -D "src" -o po/$NAME.pot;;
+        * | --help) echo "\
+Usage:
+build.sh [COMMAND/--OPTION]
+
+Commands:
+        install    install this package
+        pot        generate .pot file in po/
+        help       show help";;
+esac
